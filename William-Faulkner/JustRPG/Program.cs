bfc07d4c8553feba115fd7625d426ebd94bae9c6
drumev <milan_drumev@yahoo.com>
2014-02-22 12:13:20 +0200

    added power ups DRUMEV

--- William-Faulkner/JustRPG/Program.cs
+++ William-Faulkner/JustRPG/Program.cs
@@ -1,11 +1,22 @@
 ﻿namespace JustRpg
 {
     using System;
+    using System.IO;
 
     class Program
     {
         static void Main(string[] args)
         {
+            using(StreamReader sr = new StreamReader("../../Art/Intro.txt"))
+            {
+                var x = sr.ReadLine();
+
+                while(x != null)
+                {
+                    Console.WriteLine(x);
+                    x = sr.ReadLine();
+                }
+            }
         }
     }
 }
--- William-Faulkner/JustRpg/Engine/Engine.cs
+++ William-Faulkner/JustRpg/Engine/Engine.cs
@@ -1,7 +1,19 @@
 ﻿namespace JustRpg.Engine
 {
     using System;
-    class Engine
+    using System.Collections.Generic;
+
+    using JustRpg.Units;
+
+    public class Engine
     {
+        private  Renderer renderer;
+        private  List<Unit> units;
+
+        public Engine()
+        {
+            this.renderer = new Renderer();
+            this.units = new List<Unit>();
+        }
     }
 }
